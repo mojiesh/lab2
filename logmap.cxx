1e07f1db531791d47f4c5bbb4fb8c716f3f21c0d
mojiesh <mojtaba.eshraghi@hhu.de>
2015-11-02 11:51:01 +0100

    This code will generate logistic map!

--- logmap.cxx
+++ logmap.cxx
@@ -12,17 +12,15 @@ int main(){
   double x0;
   double dr = 0.01;
   double x1;
-  
   for (double r = 0.0; r <= 4.0; r += dr){
     x0=0.5;
     for (int n = 1; n <= 100; n++){
       x1 = r * x0 * (1-x0);
-      x0=x1;
+      x0 = x1;
       if (n >= 20){
 	cout << r << "  " << x1 <<endl;
       }
     }
   }
-  
   return 0;
 }
