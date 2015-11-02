6a80b942206495b114b90baff774797aa05e5a38
mojiesh <mojtaba.eshraghi@hhu.de>
2015-11-02 11:34:42 +0100

    s code will generate kogistic map!

---
+++ logmap.cxx
@@ -0,0 +1,28 @@
+/* Author : MOJI
+Winter 2015/16
+Lab Work 2
+Logistic Map
+03.11.2015
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  double x0;
+  double dr = 0.01;
+  double x1;
+  
+  for (double r = 0.0; r <= 4.0; r += dr){
+    x0=0.5;
+    for (int n = 1; n <= 100; n++){
+      x1 = r * x0 * (1-x0);
+      x0=x1;
+      if (n >= 20){
+	cout << r << "  " << x1 <<endl;
+      }
+    }
+  }
+  
+  return 0;
+}
